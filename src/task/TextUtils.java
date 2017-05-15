d19aa4cd5e43133e271af23cbe157acdbc38943a
Valentina M <allthemore@yandex.ru>
2017-05-15 22:22:03 +0300

    Solve TextUtils

--- src/task/TextUtils.java
+++ src/task/TextUtils.java
@@ -15,7 +15,7 @@ public class TextUtils {
 //        TODO find mistake and correct it, so the method works correctly
         String[] words = getWords(text);
         String[] uniqueWords = getUniqueWords(words);
-        return sortWords(words);
+        return sortWords(uniqueWords);
     }
     /**
      * @param text
@@ -27,32 +27,75 @@ public class TextUtils {
     public static String[] getWords(String text) {
 
 //        TODO write your code here
-
-
-        return null;
+        if (text == null || text.length() == 0) {
+            return new String[0];
+        }
+        return text.split("; ");
     }
+
+
     /**
      * @param words array with words
      * @return  array with unique words
      * */
     public static String[] getUniqueWords(String[] words) {
-
-//        TODO write your code here
-
-        return null;
+        if (words == null || words.length == 0) return words;
+        return copyUniqueWords(words);
     }
 
-     /**
+    public static String[] copyUniqueWords(String[] array) {
+        String[] dstArray = new String[array.length];
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (!contains(dstArray, array[i])) {
+                dstArray[counter++] = array[i];
+            }
+        }
+        return trimArray(dstArray, counter);
+    }
+
+    public static String[] trimArray(String[] array, int length) {
+        String[] trimmedArray = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            trimmedArray[i] = array[i];
+        }
+        return trimmedArray;
+    }
+
+    public static boolean contains(String[] array, String word) {
+        for (int i = 0; i < array.length; i++) {
+            if (word != null && word.equals(array[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
      * @param  uniqueWords array with unique words
      * @return sorted by natural order array of unique words
      * */
     public static String[] sortWords(String[] uniqueWords) {
 
 //        TODO write your code here
+        boolean sorted = false;
+        String tmp;
 
+        while (!sorted) {
+            sorted = true;
+            for (int i = 1; i < uniqueWords.length; i++) {
+// compareTo() returns a value less than 0 if the argument is a string lexicographically greater than this string
+                if (uniqueWords[i-1].compareTo(uniqueWords[i]) > 0) {
+                    sorted = false;
+                    tmp = uniqueWords[i-1];
+                    uniqueWords[i-1] = uniqueWords[i];
+                    uniqueWords[i] = tmp;
+                }
+            }
+        }
 
-
-
-        return null;
+        return uniqueWords;
     }
 }
